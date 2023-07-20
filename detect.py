b4ce3740aa176e570e6cada54e44a5467e5394eb
Lukasz Borecki <57598625+lukebor@users.noreply.github.com>
2023-07-20 08:33:20 +0200

    Update detect.py
    Signed-off-by: Lukasz Borecki <57598625+lukebor@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -79,6 +79,7 @@ def run(
         half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
         vid_stride=1,  # video frame-rate stride
+        tifmode = False,
 ):
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
@@ -108,7 +109,7 @@ def run(
     elif screenshot:
         dataset = LoadScreenshots(source, img_size=imgsz, stride=stride, auto=pt)
     else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride, tif_mode=tifmode)
     vid_path, vid_writer = [None] * bs, [None] * bs
 
     # Run inference
@@ -245,6 +246,7 @@ def parse_opt():
     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
     parser.add_argument('--vid-stride', type=int, default=1, help='video frame-rate stride')
+    parser.add_argument('--tifmode', action='store_true', help='tifmode')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     print_args(vars(opt))
